240dc21161ca4d253ab469fa65c29cecfe265850
Richard Klancer <rpk@pobox.com>
2010-08-06 19:39:43 -0400

    ActivityPage has_many ActivitySteps; change uri -> url in models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ActiveRecord::Base
   has_many :activity_pages
 
   # for a web service, URIs are *data* not just some accident of the view layer.
-  def uri
+  def url
     activity_path(self)
   end
   
@@ -12,7 +12,7 @@ class Activity < ActiveRecord::Base
   # :)
   
   def as_json(options={})
-    super({ :methods => :uri }).merge(:pages => self.activity_pages.map { |p| p.uri })
+    super({ :methods => :url }).merge(:pages => self.activity_pages.map { |p| p.url })
   end
 
 end
--- app/models/activity_page.rb
+++ app/models/activity_page.rb
@@ -1,13 +1,14 @@
 include ActionController::UrlWriter
 class ActivityPage < ActiveRecord::Base
   belongs_to :activity
+  has_many :activity_steps
   
-  def uri
+  def url
     activity_page_path(self.activity, self)
   end
   
   def as_json(options={})
-    super({ :methods => :uri })
+    super({ :methods => :url }).merge(:steps => self.activity_steps.map { |s| s.url })
   end
   
 end
