7b5ccf8e386d160f5c99e80e962a9585060edf2f
Richard Klancer <rpk@pobox.com>
2010-08-06 10:13:53 -0400

    Wiped out everything

--- db/migrate/20100805185614_rename_guide_page_sequence_to_activity.rb
+++
@@ -1,9 +0,0 @@
-class RenameGuidePageSequenceToActivity < ActiveRecord::Migration
-  def self.up
-    rename_table :guide_page_sequences, :activities
-  end
-
-  def self.down
-    rename_table :activities, :guide_page_sequences
-  end
-end
