ae07cd7ac020b230c3f6adfc324c1da188327a2b
Richard Klancer <rpk@pobox.com>
2010-08-06 02:41:10 -0400

    Flesh out ActivityPages controller and templates

--- app/controllers/activity_pages_controller.rb
+++ app/controllers/activity_pages_controller.rb
@@ -1,16 +1,58 @@
 class ActivityPagesController < ApplicationController
+
+  before_filter :find_activity
+  
   def index
-    @activity = Activity.find(params[:activity_id])
     @pages = @activity.activity_pages
   end
 
-  def show
+  def new
+    @page = ActivityPage.new(:activity => @activity)
+  end
+  
+  def update
+    @page = @activity.activity_pages.find(params[:id])
+    
+    respond_to do |format|
+      if @page.update_attributes(params[:activity_page])
+        format.html { redirect_to(:action => 'show', :activity_id => @activity.id, :id => @page.id, :notice => 'Activity was successfully updated.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def create
+    @page = @activity.activity_pages.new(params[:activity_page])
+    
+    respond_to do |format|
+      if @page.save
+        format.html { redirect_to(:action => 'show', :activity_id => @activity.id, :id => @page.id, :notice => 'Activity was successfully created.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
 
-  def new
+  def show
+    @page = @activity.activity_pages.find(params[:id])
   end
 
   def edit
+    @page = @activity.activity_pages.find(params[:id])
   end
+  
+  def destroy
+    @page = @activity.activity_pages.find(params[:id])    
+    @page.destroy
+    respond_to do |format|
+      format.html { redirect_to(:controller => 'activities', :id => @activity.id, :action => 'show', :notice => 'Page was deleted.') }
+    end
+  end
+  
+  private
+    def find_activity
+      @activity = Activity.find(params[:activity_id])
+    end
 
 end
